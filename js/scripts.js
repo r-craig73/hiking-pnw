c21eea136ebd5964351396e43eba08b6e0bcc95d
Kaila Goff and Ron Craig <kaila@>
2018-01-09 10:44:26 -0800

    fadeIn links in the Crater Lake section

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".click1").click(function() {
+    $(".hide-craterlake1").fadeIn();
+  });
+});
+
+$(document).ready(function() {
+  $(".click2").click(function() {
+    $(".hide-craterlake2").fadeIn();
+  });
+});
